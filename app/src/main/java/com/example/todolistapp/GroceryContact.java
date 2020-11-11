5850b5388557733fd2a602ce4afed60bc2dffea9
Kritika0220 <kritishah2017@gmail.com>
2020-11-11 18:25:47 +0530

    Adding files

---
+++ app/src/main/java/com/example/todolistapp/GroceryContact.java
@@ -0,0 +1,15 @@
+package com.example.todolistapp;
+
+import android.provider.BaseColumns;
+
+public class GroceryContact {
+
+    private GroceryContact() {}
+
+    public static final class GroceryEntry implements BaseColumns{
+        public static final String TABLE_NAME = "groceryList";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_AMOUNT = "amount";
+        public static final String COLUMN_TIMESTAMP = "timestamp";
+    }
+}
---
+++ app/src/main/java/com/example/todolistapp/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.todolistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase mDatabase;
+    private GroceryAdapter mAdapter;
+    private EditText mEditTextName;
+    private TextView mTextViewAmount;
+    private int mAmount = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GroceryDBHelper dbHelper = new GroceryDBHelper(this);
+        mDatabase = dbHelper.getWritableDatabase();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new GroceryAdapter(this, getAllItems());
+        recyclerView.setAdapter(mAdapter);
+
+        mEditTextName = findViewById(R.id.edittext_name);
+        mTextViewAmount = findViewById(R.id.textview_amount);
+
+        Button buttonIncrease = findViewById(R.id.button_increase);
+        Button buttonDecrease = findViewById(R.id.button_decrease);
+        Button buttonAdd = findViewById(R.id.button_add);
+
+        buttonIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increase();
+            }
+        });
+
+        buttonDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decrease();
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+    }
+
+    private void increase() {
+        mAmount++;
+        mTextViewAmount.setText(String.valueOf(mAmount));
+    }
+
+    private void decrease() {
+        if (mAmount > 0) {
+            mAmount--;
+            mTextViewAmount.setText(String.valueOf(mAmount));
+        }
+    }
+
+    private void addItem() {
+
+        if(mEditTextName.getText().toString().trim().length() == 0 || mAmount == 0) {            //trim will remove empty spaces at the start and end
+            return;
+        }
+
+        String name = mEditTextName.getText().toString();
+        ContentValues cv = new ContentValues();
+        cv.put(GroceryContact.GroceryEntry.COLUMN_NAME, name);
+        cv.put(GroceryContact.GroceryEntry.COLUMN_AMOUNT, mAmount);
+
+        mDatabase.insert(GroceryContact.GroceryEntry.TABLE_NAME, null, cv);
+        mAdapter.swapCursor(getAllItems());
+
+        mEditTextName.getText().clear();
+    }
+
+    private Cursor getAllItems() {
+        return mDatabase.query(
+                GroceryContact.GroceryEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                GroceryContact.GroceryEntry.COLUMN_TIMESTAMP + " DESC"
+        );
+    }
+}
